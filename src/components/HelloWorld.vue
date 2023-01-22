4f5f8dd872cac90ae5ca9350b84bdfa5540a6b54
rCatMan <rCatMan@outlook.com>
2023-01-22 19:11:08 +0800

    feat:support tailwindcss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,16 +4,24 @@ import { ref } from 'vue'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+
+const clickCount = () => {
+  count.value++
+  console.log(count)
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="clickCount">count is {{ count }}</button>
     <p>
-      Edit
+      
       <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+    <p class="text-red-500">
+      Hello rCatMan
     </p>
   </div>
 
