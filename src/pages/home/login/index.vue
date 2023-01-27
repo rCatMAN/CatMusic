f130c364065ad47d2194dc33147ab1546374d4e5
rCatMan <rCatMan@outlook.com>
2023-01-28 05:48:58 +0800

    feat : add switchLoginMode Button

--- src/pages/home/login/index.vue
+++ src/pages/home/login/index.vue
@@ -17,45 +17,50 @@
                 </div>
             </div>
             <div class="form flex flex-col items-center relative top-7 rounded-3xl" style="width: 400px;height: 480px;">
-                <div class="switch-login-mode absolute flex items-center justify-center cursor-pointer">
+                <div @click="switchLoginMde"
+                    class="switch-login-mode absolute flex items-center justify-center cursor-pointer">
                     <span class="font-semibold text-xs">Switch LoginMode</span>
                 </div>
-                <div class=" mt-12 relative">
-                    <input v-model="phoneNum" class="select-text" type="text" id="phone">
-                    <label for="phone" class="absolute text-gray-500 font-bold text-sm cursor-text duration-150"
-                        style="left: 10%;transform: translateY(-50%);"
-                        :style="{ top: phoneNum ? '-30%' : '50%', color: phoneNum ? 'var(--primary-color)' : '' }">Phone</label>
-                    <div v-show="loginMode === 0"
-                        class="send-code-button absolute flex items-center justify-center cursor-pointer">
-                        <span class="font-semibold text-xs">Send Code</span>
-                    </div>
-                </div>
-                <div class=" mt-8 relative">
-                    <input v-model="password" class="select-text" type="password" id="password">
-                    <label for="password" class="absolute text-gray-500 font-bold text-sm cursor-text duration-150"
-                        style="left: 10%;transform: translateY(-50%);"
-                        :style="{ top: password ? '-30%' : '50%', color: password ? 'var(--primary-color)' : '' }">PassWord</label>
-                </div>
-                <div @click="login" class="flex items-center justify-center cursor-pointer mt-16 login-button "
-                    style="">
-                    <span class="font-semibold">Sign In</span>
-                </div>
-                <div class="options mt-12" style="width: 300px;">
-                    <div
-                        class="separator relative w-full flex items-center justify-center text-sm font-extralight text-gray-500">
-                        <span>or continue with</span>
-                    </div>
-                    <div class="mt-8 flex items-center justify-around" style="width: 300px;">
-                        <div class="icon-box">
-                            <SvgIcon iconClass="vx" />
-                        </div>
-                        <div class="icon-box">
-                            <SvgIcon iconClass="qq" />
-                        </div>
-                        <div class="icon-box">
-                            <SvgIcon iconClass="sina" />
+                <div v-show="loginMode === 1">
+                    <div class=" mt-12 relative">
+                        <input v-model="phoneNum" class="select-text" type="text" id="phone">
+                        <label for="phone" class="absolute text-gray-500 font-bold text-sm cursor-text duration-150"
+                            style="left: 10%;transform: translateY(-50%);"
+                            :style="{ top: phoneNum ? '-30%' : '50%', color: phoneNum ? 'var(--primary-color)' : '' }">Phone</label>
+                        <div v-show="loginMode === 1"
+                            class="send-code-button absolute flex items-center justify-center cursor-pointer">
+                            <span class="font-semibold text-xs">Send Code</span>
                         </div>
                     </div>
+                    <div class=" mt-8 relative">
+                        <input v-model="password" class="select-text" type="password" id="password">
+                        <label for="password" class="absolute text-gray-500 font-bold text-sm cursor-text duration-150"
+                            style="left: 10%;transform: translateY(-50%);"
+                            :style="{ top: password ? '-30%' : '50%', color: password ? 'var(--primary-color)' : '' }">PassWord</label>
+                    </div>
+                    <div @click="login" class="flex items-center justify-center cursor-pointer mt-16 login-button "
+                        style="">
+                        <span class="font-semibold">Sign In</span>
+                    </div>
+                    <div class="options mt-12" style="width: 300px;">
+                        <div
+                            class="separator relative w-full flex items-center justify-center text-sm font-extralight text-gray-500">
+                            <span>or continue with</span>
+                        </div>
+                        <div class="mt-8 flex items-center justify-around" style="width: 300px;">
+                            <div class="icon-box">
+                                <SvgIcon iconClass="vx" />
+                            </div>
+                            <div class="icon-box">
+                                <SvgIcon iconClass="qq" />
+                            </div>
+                            <div class="icon-box">
+                                <SvgIcon iconClass="sina" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div v-show="loginMode === 2">
                 </div>
             </div>
         </div>
@@ -76,7 +81,12 @@ const router = useRouter()
 const phoneNum = ref<string>()
 const password = ref<number>()
 const loginMode = ref(LoginMode.PhoneAndPassword)
-
+const switchLoginMde = () => {
+    loginMode.value++
+    if (loginMode.value > LoginMode.QrCode) {
+        loginMode.value = LoginMode.PhoneAndPassword
+    }
+}
 const login = () => {
     if (password.value && phoneNum.value) {
         phoneLoginApi(phoneNum.value, password.value).then((res) => {
@@ -86,7 +96,6 @@ const login = () => {
         alert("账号或密码不能空")
     }
 }
-
 </script>
 
 <style scoped>
